23932b908a2395913df3200d78d82c1a0d9b060f
brian033 <85883730+brian033@users.noreply.github.com>
2024-03-13 02:12:18 +0800

    finished all the problems

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,16 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
-
+    // add a constructor 
+    bool private called;
+    
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if(!called){
+            called = true;
+            return 1000;
+        }
+        return 123;
     }
 }
 
@@ -16,14 +23,26 @@ interface IClassroomV2 {
 }
 
 contract StudentV2 {
-    function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+    function register() external view returns (uint256){
+        if(!IClassroomV2(msg.sender).isEnrolled()){
+            return 1000;
+        }else{
+            return 123;
+        }
     }
 }
 
+interface IClassroomV3 {
+    function isEnrolled() external view returns (bool);
+    function code() external view returns (uint256);
+}
 /* Problem 3 Interface & Contract */
 contract StudentV3 {
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+        if(gasleft() == 7194){
+            return 1000;
+        }else{
+            return 123;
+        }
     }
 }
